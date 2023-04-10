a539bd7225fa587e369d89d544bf0165fb9f8c82
Tristanstormmm <113917115+Tristanstormmm@users.noreply.github.com>
2023-04-10 13:00:09 +0200

    Add files via upload

---
+++ src/Timeline.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import Linechart from "./Graphs/Linechart";
+
+const Timeline = () => {
+    return (
+        <div>
+            <Linechart />
+        </div>
+    )
+}
+
+export default Timeline
